961027746dfcae80ea6d83eb695404320d2ff080
Muhammadrasul <muhammadrasul446@mail.ru>
2020-05-10 03:17:14 +0500

    Bug fixes, update README

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Neezy
-Neezy website template. Source: Mudaki.uz
+# Neezy 
 
-This website was supposed to be for Neezy website builders' company, but they were fucking annoying, and i decided to make it open source.
+![Neezy](https://user-images.githubusercontent.com/64916997/81486187-809b3500-926c-11ea-8814-3f3903172274.png)
+
+### Clone the repository and feel free to use
+
+> `git clone https://github.com/Muhammadrasul446/Neezy`
+
+### My Contacts
+
+[Twitter](https://twitter.com/A_M_R_4_4_6)
+[Stackoverflow](https://stackoverflow.com/users/13490404/muhammadrasul)
+[Linkedin](https://www.linkedin.com/in/muhammadrasul-abdulhayev-6644821a9/)
+[Sololearn](https://www.sololearn.com/Profile/13162535)
+[Telegram](https://t.me/A_M_R_4_4_6)
--- js/index_script.js
+++ js/index_script.js
@@ -1,48 +1,51 @@
-
+// Needed variables
 let drop_down_menu = document.getElementById('drop');
-
-let open = false;
-
-function toggleMenu() {
-    if(open) {
-        drop_down_menu.style.display = "none";
-        drop_down_menu.style.opacity = "0";
-        open = !open;
-    } else if (!open) {
-        drop_down_menu.style.display = "block";
-        drop_down_menu.style.opacity = "1";
-        open = !open;
-    }
-}
-
 let checkbox = document.getElementById("mode");
 let style = document.getElementById("style");
+let open = false;
 
+// reload the page
+function reload() {
+	location.reload();
+}
+
+// hide/show the settings menu
+function toggleMenu() {
+	if(open) {
+		drop_down_menu.style.display = "none";
+		drop_down_menu.style.opacity = "0";
+		open = !open;
+	} else if (!open) {
+		drop_down_menu.style.display = "block";
+		drop_down_menu.style.opacity = "1";
+		open = !open;
+	}
+}
+
+// Switch dark/light modes
 dark = false;
 
 if (checkbox.checked) {
-    dark = true;
+	dark = true;
 } else {
-    dark = false;
+	dark = false;
 }
 
 if (dark) {
-    style.removeAttribute("href");
-    style.setAttribute("href", "css/index_dark.css");
+	style.removeAttribute("href");
+	style.setAttribute("href", "css/index_dark.css");
 } else if (!dark) {
-    style.removeAttribute("href");
-    style.setAttribute("href", "css/index_light.css");
+	style.removeAttribute("href");
+	style.setAttribute("href", "css/index_light.css");
 }
 
-function reload() {
-    location.reload();
-}
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
 });
